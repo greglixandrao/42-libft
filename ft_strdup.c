fed11ae17ac187da1ac1403354db43a49b26f091
Greg Lixandrão <gtreviza@student.42sp.org.br>
2022-09-07 19:30:35 +0200

    add ft_strdup.c file

---
+++ ft_strdup.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/07 19:05:41 by coder             #+#    #+#             */
+/*   Updated: 2022/09/07 19:27:48 by coder            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+/*#include <string.h>
+#include <stdio.h>*/
+
+int	ft_strlen(char *s);
+
+char	*ft_strdup(char *src)
+{
+	char	*new;
+	int		i;
+
+	new = (char *)malloc(sizeof(char) * ft_strlen(src) + 1);
+	i = 0;
+	if (!new)
+		return (NULL);
+	while (*src)
+		new[i++] = *src++;
+	new[i] = '\0';
+	return (new);
+}
+/*
+int	main(int argc, char **argv)
+{
+	char	*meu_strdup;
+	char	*original_strdup;
+
+	if (argc == 2)
+	{
+		meu_strdup = ft_strdup(argv[1]);
+		original_strdup = strdup(argv[1]);
+		printf(":%s:\n:%s:\n", meu_strdup, original_strdup);
+	}
+	return (0);
+}*/
