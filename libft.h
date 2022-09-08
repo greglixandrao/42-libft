23def8845c058a5318fb0cc09d1aa883743e7d75
Greg Lixandrão <gtreviza@student.42sp.org.br>
2022-09-08 02:37:32 +0200

    add libft.h file to libft project

---
+++ libft.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/07 21:15:24 by coder             #+#    #+#             */
+/*   Updated: 2022/09/07 23:50:44 by coder            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_strlen(char *str);
+int		ft_atoi(char *str);
+char	*ft_strdup(char *src);
+
+#endif
