034777a9a4efbc916234a37c6dc840924ea1bd86
Greg Lixandrão <gtreviza@student.42sp.org.br>
2022-09-10 23:08:53 +0200

    add correct ft_strlen ft_atoi ft_strdup prototype

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gtreviza <gtreviza@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 21:15:24 by coder             #+#    #+#             */
-/*   Updated: 2022/09/10 01:18:19 by gtreviza         ###   ########.fr       */
+/*   Updated: 2022/09/10 18:20:13 by gtreviza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,8 @@ int ft_isprint(int c);
 int ft_toupper(int c);
 int ft_tolower(int c);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
-int ft_strlen(char *str);
-int ft_atoi(char *str);
-char *ft_strdup(char *src);
+size_t ft_strlen(const char *str);
+int ft_atoi(const char *str);
+char *ft_strdup(const char *s);
 
 #endif
