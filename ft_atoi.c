369fb0241a1174e60d47688b4e61be07ee35024c
Greg Lixandrão <gtreviza@student.42sp.org.br>
2022-09-10 23:06:31 +0200

    Add ft_atoi finished and tested

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,34 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*   By: gtreviza <gtreviza@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 04:37:30 by gtreviza          #+#    #+#             */
-/*   Updated: 2022/09/07 21:26:31 by coder            ###   ########.fr       */
+/*   Updated: 2022/09/10 23:03:56 by gtreviza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_atoi(char *str)
+#include <stdlib.h>
+#include <stdio.h>
+
+int ft_atoi(const char *str)
 {
-	int	result;
-	int	count;
+	// printf("<<%s>>\n\n", str);
+	int result;
+	int sign;
+	int i;
 
 	result = 0;
-	count = 1;
-	while ((*str >= 9 && *str <= 13) || *str == ' ')
-		str++;
-	while (*str && (*str == '+' || *str == '-'))
+	sign = 1;
+	i = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
 	{
-		if (*str == '-')
-			count *= -1;
-		str++;
+		if (str[i] == '-')
+			sign = -1;
+		i++;
 	}
-	while (*str && *str >= '0' && *str <= '9')
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
 	{
-		result *= 10;
-		result += *str - '0';
-		str++;
+		result = result * 10 + (str[i] - '0');
+		i++;
 	}
-	result *= count;
-	return (result);
+	return (result * sign);
 }
