750d75c0d56744bc45e0c7d36a3d454fc49744c4
Greg Lixandrão <gtreviza@student.42sp.org.br>
2022-09-10 02:11:35 +0200

    Complementing with new function, strncmp toupper tolower

--- libft.h
+++ libft.h
@@ -3,26 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*   By: gtreviza <gtreviza@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 21:15:24 by coder             #+#    #+#             */
-/*   Updated: 2022/09/07 23:50:44 by coder            ###   ########.fr       */
+/*   Updated: 2022/09/10 01:18:19 by gtreviza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-# define LIBFT_H
+#define LIBFT_H
 
-# include <unistd.h>
-# include <stdlib.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_strlen(char *str);
-int		ft_atoi(char *str);
-char	*ft_strdup(char *src);
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
+int ft_toupper(int c);
+int ft_tolower(int c);
+int ft_strncmp(const char *s1, const char *s2, size_t n);
+int ft_strlen(char *str);
+int ft_atoi(char *str);
+char *ft_strdup(char *src);
 
 #endif
