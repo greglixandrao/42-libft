9f093002118440cb4b634d2ecc9544f313961b89
Greg Lixandrão <gtreviza@student.42sp.org.br>
2022-09-10 02:13:07 +0200

    Add ft_strncmp.c file

---
+++ ft_strncmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gtreviza <gtreviza@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/09 20:01:53 by gtreviza          #+#    #+#             */
+/*   Updated: 2022/09/10 01:45:12 by gtreviza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	unsigned int i;
+
+	i = 1;
+	if (n == 0)
+		return (0);
+	while (*s1 && i < n)
+	{
+		if (*s1 != *s2)
+		{
+			break;
+		}
+		s1++;
+		s2++;
+		i++;
+	}
+	return (*(const unsigned char *)s1 - *(const unsigned char *)s2);
+}
