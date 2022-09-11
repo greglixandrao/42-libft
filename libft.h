cfe3dbbfeb80f6e4fd6ca1dc9f11930d884bf8b2
Greg Lixandrão <gtreviza@student.42sp.org.br>
2022-09-12 00:46:11 +0200

    add ft_strlcpy to libft.h and Makefile

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gtreviza <gtreviza@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 21:15:24 by coder             #+#    #+#             */
-/*   Updated: 2022/09/10 23:29:43 by gtreviza         ###   ########.fr       */
+/*   Updated: 2022/09/12 00:44:29 by gtreviza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,9 @@ int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-size_t	ft_strlen(const char *str);
 int		ft_atoi(const char *str);
 char	*ft_strdup(const char *s);
+size_t	ft_strlen(const char *str);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 
 #endif
