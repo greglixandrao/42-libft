58f41580646955f35a7221ff17b3b8b102e824df
Greg Lixandrão <gtreviza@student.42sp.org.br>
2022-09-14 03:50:51 +0200

    add strchr strnstr and atrrchr to libft.h Makefile

--- libft.h
+++ libft.h
@@ -6,13 +6,14 @@
 /*   By: gtreviza <gtreviza@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 21:15:24 by coder             #+#    #+#             */
-/*   Updated: 2022/09/12 00:56:44 by gtreviza         ###   ########.fr       */
+/*   Updated: 2022/09/14 03:48:11 by gtreviza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <stdio.h> // TIRAR ESSA MERDA ANTES DE ENVIAR
 # include <unistd.h>
 # include <stdlib.h>
 
@@ -26,6 +27,8 @@ int		ft_tolower(int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_atoi(const char *str);
 char	*ft_strdup(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 size_t	ft_strlen(const char *str);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
