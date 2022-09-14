eaa54e4e388a63633e335847cc0381a2200244fa
Greg Lixandrão <gtreviza@student.42sp.org.br>
2022-09-14 02:17:58 +0200

    finished ft_strlcat tested and norminetted

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: gtreviza <gtreviza@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 00:54:27 by gtreviza          #+#    #+#             */
-/*   Updated: 2022/09/12 01:48:38 by gtreviza         ###   ########.fr       */
+/*   Updated: 2022/09/14 02:16:39 by gtreviza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,22 @@
 
 size_t	ft_strlen(const char *str);
 
-size_t	ft_strlcat(char *restrict dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	count;
 	size_t	src_size;
 	size_t	dst_size;
 	size_t	src_index;
 
 	src_size = ft_strlen(src);
 	dst_size = ft_strlen(dst);
-	count = dst_size;
 	src_index = 0;
-	while ((*(src + src_index) != '\0'))
+	if (size <= dst_size)
+		return (size + src_size);
+	while ((*(src + src_index) != '\0') && (dst_size + src_index) < size - 1)
 	{
-		*(dst + count) = *(src + src_index);
-		count++;
+		*(dst + dst_size + src_index) = *(src + src_index);
 		src_index++;
-		if (count == size - 1)
-			break ;
 	}
-	if (size == 1 || *src == 0)
-		*(dst + count) = '\0';
+	*(dst + dst_size + src_index) = '\0';
 	return (dst_size + src_size);
 }
