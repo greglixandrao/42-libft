415b17d8befee06e75e3c59742e44b6462b7af50
Greg Lixandrão <contato@greglixandrao.com>
2022-09-14 23:55:03 -0300

    update strrchr with ft_strlen to find last_element

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: gtreviza <gtreviza@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 03:39:23 by gtreviza          #+#    #+#             */
-/*   Updated: 2022/09/14 03:48:19 by gtreviza         ###   ########.fr       */
+/*   Updated: 2022/09/14 21:42:50 by gtreviza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,12 @@ size_t	ft_strlen(const char *str);
 
 char	*ft_strrchr(const char *s, int c)
 {
-	while (*s)
-		s++;
+	int last_element;
+
+	last_element = ft_strlen(s);
 	if (!*s || c < -128 || c > 127)
 		return ((char *) s);
-	while (*s != c)
+	while (*(s + last_element) != c)
 		s--;
 	if (*s == c)
 		return ((char *) s);
