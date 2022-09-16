596eac3989454f794f154f73149a1cd8c36da2c0
Greg Lixandrão <gtreviza@student.42sp.org.br>
2022-09-16 04:17:50 +0200

    fixed ft_strchr.c file

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,17 +6,17 @@
 /*   By: gtreviza <gtreviza@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 03:11:17 by gtreviza          #+#    #+#             */
-/*   Updated: 2022/09/14 03:46:39 by gtreviza         ###   ########.fr       */
+/*   Updated: 2022/09/16 04:08:35 by gtreviza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strrchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
 	if (!*s || c < -128 || c > 127)
 		return ((char *) s);
-	while (*s != c && *s != '\0')
+	while (*s != c && *s)
 		s++;
 	if (*s == c)
 		return ((char *) s);
