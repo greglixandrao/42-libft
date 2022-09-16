24f3684e58d830754f4f02e15a7e811b2aab2a78
Greg Lixandrão <gtreviza@student.42sp.org.br>
2022-09-16 04:18:12 +0200

    not finish ft_strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: gtreviza <gtreviza@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 03:39:23 by gtreviza          #+#    #+#             */
-/*   Updated: 2022/09/14 21:42:50 by gtreviza         ###   ########.fr       */
+/*   Updated: 2022/09/16 04:15:24 by gtreviza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@ size_t	ft_strlen(const char *str);
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int last_element;
+	int	last_element;
 
 	last_element = ft_strlen(s);
 	if (!*s || c < -128 || c > 127)
 		return ((char *) s);
-	while (*(s + last_element) != c)
+	while (*(s + (last_element - 1)) != c)
 		s--;
-	if (*s == c)
+	if (*s == (unsigned char) c)
 		return ((char *) s);
 	else
 		return ((char *) NULL);
