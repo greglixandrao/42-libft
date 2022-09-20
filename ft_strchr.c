334ae73ba388213520c1b424ba5351e13d318be7
Greg Lixandrão <gtreviza@student.42sp.org.br>
2022-09-21 01:27:01 +0200

    modify condition ft_strnstr include cast for int c

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: gtreviza <gtreviza@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 03:11:17 by gtreviza          #+#    #+#             */
-/*   Updated: 2022/09/16 04:08:35 by gtreviza         ###   ########.fr       */
+/*   Updated: 2022/09/18 22:53:15 by gtreviza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	if (!*s || c < -128 || c > 127)
-		return ((char *) s);
+	c = (char) c;
 	while (*s != c && *s)
 		s++;
 	if (*s == c)
 		return ((char *) s);
 	else
 		return ((char *) NULL);
+	if (!*s)
+		return ((char *) s);
 }
