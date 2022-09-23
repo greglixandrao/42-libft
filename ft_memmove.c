1bd93286d86068bfedcc6ff21b744bd49a743793
Greg Lixandrão <gtreviza@student.42sp.org.br>
2022-09-23 04:12:18 +0200

    add ft_memmove to project

---
+++ ft_memmove.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gtreviza <gtreviza@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/23 03:12:06 by gtreviza          #+#    #+#             */
+/*   Updated: 2022/09/23 04:08:28 by gtreviza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	if (src > dest)
+		ft_memcpy(dest, src, n);
+	else if (src < dest && n > 0)
+	{
+		i = n;
+		while (i-- > 0)
+			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+	}
+	return (dest);
+}
