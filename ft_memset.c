c415dc996c0f6926b82a13e368c1b1afb4c930f9
Greg Lixandrão <gtreviza@student.42sp.org.br>
2022-09-23 04:11:58 +0200

    add ft_memset to project

---
+++ ft_memset.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gtreviza <gtreviza@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/21 01:33:56 by gtreviza          #+#    #+#             */
+/*   Updated: 2022/09/21 01:53:06 by gtreviza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*str;
+
+	str = s;
+	while (n--)
+	{
+		*str = (unsigned char) c;
+		str++;
+	}
+	return (s);
+}
