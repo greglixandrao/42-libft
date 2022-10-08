4886a2f997c0cc756cd47c8d29e37759a49b0799
Greg Lixandrão <gtreviza@student.42sp.org.br>
2022-10-08 05:23:46 +0200

    update libft Makefile with bonus function

--- Makefile
+++ Makefile
@@ -6,22 +6,25 @@
 #    By: gtreviza <gtreviza@student.42sp.org.br>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/09/07 21:27:10 by coder             #+#    #+#              #
-#    Updated: 2022/10/01 23:15:54 by gtreviza         ###   ########.fr        #
+#    Updated: 2022/10/08 05:17:35 by gtreviza         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-NAME 	= libft.a
-SRCS 	= ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c \
+NAME	= libft.a
+SRCS	= ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c \
 		ft_toupper.c ft_tolower.c ft_strncmp.c ft_strlen.c ft_atoi.c \
 		ft_strdup.c ft_strlcpy.c ft_strlcat.c ft_strchr.c ft_strrchr.c \
 		ft_strnstr.c ft_memset.c ft_bzero.c ft_memcpy.c ft_memmove.c \
 		ft_memchr.c ft_memcmp.c ft_calloc.c ft_substr.c ft_strjoin.c \
 		ft_putchar_fd.c ft_putstr_fd.c ft_putendl_fd.c ft_putnbr_fd.c \
-		ft_itoa.c ft_striteri.c ft_strmapi.c ft_strtrim.c
-CC 		= gcc -c
+		ft_itoa.c ft_striteri.c ft_strmapi.c ft_strtrim.c ft_split.c
+BONUS_SRC = ft_lstnew.c ft_lstadd_front.c ft_lstsize.c ft_lstlast.c \
+		ft_lstadd_back.c ft_lstdelone.c ft_lstclear.c ft_lstiter.c ft_lstmap.c
+CC		= gcc -c
 INC 	= -I .\libft.h
-OBJ 	= ${SRCS:.c=.o}
-FLAGS 	= -Wall -Wextra -Werror
+OBJ		= ${SRCS:.c=.o}
+BONUS_OBJ	= $(BONUS_SRC:%.c=%.o)
+FLAGS	= -Wall -Wextra -Werror
 
 all: ${NAME}
 
@@ -31,11 +34,17 @@ ${NAME}: ${OBJ}
 
 ${OBJ}:
 	${CC} ${FLAGS} ${@:.o=.c} ${INC}
+	
+bonus: $(BONUS_OBJ)
+	ar -rcs ${NAME} ${BONUS_OBJ}
+	ar -t ${NAME}
 
 clean:
-	rm -f ${OBJ}
+	rm -f ${OBJ} $(BONUS_OBJ)
 
 fclean: clean
 	rm -f ${NAME}
 
 re: fclean all
+
+.PHONY: all clean fclean re bonus
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gtreviza <gtreviza@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 21:15:24 by coder             #+#    #+#             */
-/*   Updated: 2022/10/01 23:17:02 by gtreviza         ###   ########.fr       */
+/*   Updated: 2022/10/08 05:19:54 by gtreviza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+
+typedef struct s_list
+{
+	void				*content;
+	struct s_list		*next;
+}	t_list;
 
 int		ft_isascii(int c);
 int		ft_isalpha(int c);
@@ -26,6 +32,7 @@ int		ft_tolower(int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_atoi(const char *str);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_lstsize(t_list *lst);
 char	*ft_strdup(const char *s);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
@@ -34,6 +41,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_itoa(int n);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	ft_putchar_fd(char c, int fd);
@@ -45,9 +54,16 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 size_t	ft_strlen(const char *str);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+t_list	*ft_lstnew(void *content);
+t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
