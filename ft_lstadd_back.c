8a7c337b31f2c4937801e9c08ed701d7e3b68cc7
Greg Lixandrão <gtreviza@student.42sp.org.br>
2022-10-08 05:25:51 +0200

    add ft_lstadd_back to project

---
+++ ft_lstadd_back.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gtreviza <gtreviza@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/08 04:46:30 by gtreviza          #+#    #+#             */
+/*   Updated: 2022/10/08 04:47:17 by gtreviza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
+	ft_lstlast(*lst)->next = new;
+}
